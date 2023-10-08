f105116a5a3d24916af916777def1ba88e818284
revanthchandrag <revanthchandrag@gmail.com>
2023-10-08 17:57:56 -0500

    Time: 61 ms (17.75%), Space: 16.9 MB (94.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,48 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        def isAlphanumeric(char):
-            return char.isalnum()  # Check if a character is alphanumeric
+        def nonAlphaNumeric(char):
+            # print(ord('a'), ord('A'))
+            if not(ord(char) >= ord('a') and ord(char) <= ord('z')) and not(ord(char) >= ord('A') and ord(char) <= ord('Z'))  and not(ord(char) >= ord('0') and ord(char) <= ord('9')):
+                return True
+            return False
         
         l = 0
         r = len(s) - 1
-        
         while l < r:
-            while l < r and not isAlphanumeric(s[l]):
+            leftChar = None
+            rightChar = None
+            while nonAlphaNumeric(s[l]) :
                 l += 1
-            while l < r and not isAlphanumeric(s[r]):
-                r -= 1
+                if l >= len(s):
+                    return True
+            if ord(s[l])  >= ord('A') and ord(s[l]) <= ord('Z'):  
+                leftChar = chr(ord(s[l]) + 32)
+            if leftChar is None:
+                leftChar = s[l]    
+
+            while nonAlphaNumeric(s[r]):
+                r -= 1    
+                if r < 0:
+                    return True
+            if ord(s[r]) >= ord('A') and ord(s[r]) <= ord('Z'):  
+                rightChar = chr(ord(s[r]) + 32)    
+            if rightChar is None:
+                rightChar = s[r]
             
-            leftChar = s[l].lower()  # Convert to lowercase
-            rightChar = s[r].lower()
-            
+            # print (leftChar, rightChar)
             if leftChar != rightChar:
                 return False
-            
-            l += 1
-            r -= 1
-        
+            else:
+                l += 1
+                r -= 1
+    
+            """
+            if non alphabets: ord(s[i]) < ord('A') or ord(s[i]) > ord('z')
+                l+=1
+            else if capital letters:
+            elif ord(s[i]) is >= ord('A') and <= ord('Z'):
+                then we add 26 to make it small
+                    newChar = str(ord(s[i]) + 26)
+            """        
+                    
         return True
