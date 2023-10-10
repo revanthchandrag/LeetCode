f8d4a68f01d02c730194815919a89d2a2c98b504
revanthchandrag <revanthchandrag@gmail.com>
2023-10-09 22:27:04 -0500

    Time: 159 ms (22.08%), Space: 17.8 MB (49.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,26 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dictionary = {}
+        count = 0
+        candidate = nums[0]
         for num in nums:
-            if num in dictionary:
-                dictionary[num] += 1
+            if count == 0:
+                candidate = num
+                count = 1
+            elif num == candidate:
+                count += 1
             else:
-                dictionary[num] = 1
-        max_count = 0
-        majority = nums[0]
-        for key in dictionary.keys():
-            if dictionary[key] > max_count:
-                max_count = dictionary[key]
-                majority = key
-        return majority        
+                count -= 1
+        return candidate
+        # dictionary = {}
+        # for num in nums:
+        #     if num in dictionary:
+        #         dictionary[num] += 1
+        #     else:
+        #         dictionary[num] = 1
+        # max_count = 0
+        # majority = nums[0]
+        # for key in dictionary.keys():
+        #     if dictionary[key] > max_count:
+        #         max_count = dictionary[key]
+        #         majority = key
+        # return majority        
